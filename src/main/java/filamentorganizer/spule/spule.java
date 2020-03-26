5bb51ac03823367c35f6d0952772b02fafc42ad4
rohmilch <marlenar@gmx.de>
2020-03-26 19:06:43 +0100

    Eigenschaften Spule

--- src/main/java/filamentorganizer/spule/spule.java
+++ src/main/java/filamentorganizer/spule/spule.java
@@ -1,5 +1,16 @@
 package filamentorganizer.spule;
 
 public class spule {
-
+	
+	int mLength;
+	int mTotalWeigth;
+	String mColour; 
+	String mName;
+	String mManufacturer;
+	String mMaterial; //Enum
+	int mDiameter; //egal?
+	int mPrice;
+	int mIdealNoozleTemp;
+	int mIdealBedTemp;
+	
 }
