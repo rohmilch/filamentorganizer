91336ca4a7c5a16262ff67382643c6185cbf64df
rohmilch <marlenar@gmx.de>
2020-04-09 17:11:32 +0200

    universellere DB

--- src/main/java/filamentorganizer/general/Constants.java
+++ src/main/java/filamentorganizer/general/Constants.java
@@ -2,7 +2,7 @@ package filamentorganizer.general;
 
 public class Constants {
 
-	public static final String DATABASE_URL = "jdbc:sqlite:C:/sqlite/db/test.db";
+	public static final String DATABASE_URL = "jdbc:sqlite:test.db";
 	public static final String FXML_MAIN = "/MainWindow.fxml";
 	public static final String FXML_POPUP_ADD_FILAMENT = "/PopupAddFilament.fxml";
 
