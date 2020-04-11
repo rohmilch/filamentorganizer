3fb0f1f80ec084d9651686d5dca8becb9949fc3c
rohmilch <marlenar@gmx.de>
2020-04-11 14:08:06 +0200

    Nur Integer oder Double beim Formular/HInzufügen Filament

--- src/main/java/filamentorganizer/controller/ControllerForPopupAddFilament.java
+++ src/main/java/filamentorganizer/controller/ControllerForPopupAddFilament.java
@@ -14,6 +14,9 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextFormatter;
+import javafx.util.converter.DoubleStringConverter;
+import javafx.util.converter.IntegerStringConverter;
 
 public class ControllerForPopupAddFilament extends AbstractController implements Initializable {
 
@@ -49,26 +52,47 @@ public class ControllerForPopupAddFilament extends AbstractController implements
 	public void initialize(URL url, ResourceBundle rb) {
 		PUNewFilaMaterialWert.setItems(FXCollections.observableArrayList(Material.getValues()));
 		initButtonListener();
+		formatToOnlyNumericTextFields();
+		PUNewFilaDurchmWert.setText("1.75");
+		PUNewFilaMaterialWert.getSelectionModel().select(Material.PLA);
+	}
+
+	private void formatToOnlyNumericTextFields() {
+		PUNewFilaGewichtWert.setTextFormatter(new TextFormatter(new IntegerStringConverter()));
+		PUNewFilaNozTempWert.setTextFormatter(new TextFormatter(new IntegerStringConverter()));
+		PUNewFilaPreisWert.setTextFormatter(new TextFormatter(new DoubleStringConverter()));
+		PUNewFilaDurchmWert.setTextFormatter(new TextFormatter(new DoubleStringConverter()));
+		PUNewFilaLaengWert.setTextFormatter(new TextFormatter(new IntegerStringConverter()));
+		PUNewFilaBedTempWert.setTextFormatter(new TextFormatter(new IntegerStringConverter()));
 	}
 
 	private void initButtonListener() {
 		mOKButton.setOnMouseClicked(new EventHandler<Event>() {
 
 			public void handle(Event pEvent) {
-				FilamentSpool lNewSpool = new FilamentSpool();
-				lNewSpool.setName(PUNewFilaNameWert.getText());
-				lNewSpool.setColour(PUNewFilaFarbeWert.getText());
-				lNewSpool.setManufacturer(PUNewFilaHerstellerWert.getText());
-				lNewSpool.setIdealNoozleTemp(Integer.parseInt(PUNewFilaNozTempWert.getText()));
-				lNewSpool.setIdealBedTemp(Integer.parseInt(PUNewFilaBedTempWert.getText()));
-				lNewSpool.setWeigth(Integer.parseInt(PUNewFilaGewichtWert.getText()));
-				lNewSpool.setPrice(Integer.parseInt(PUNewFilaPreisWert.getText()));
-				lNewSpool.setDiameter(Integer.parseInt(PUNewFilaDurchmWert.getText()));
-				lNewSpool.setLength(Integer.parseInt(PUNewFilaLaengWert.getText()));
-				lNewSpool.setMaterial((Material) PUNewFilaMaterialWert.getSelectionModel().getSelectedItem());
+				if (!(PUNewFilaNameWert.getText().isEmpty() && PUNewFilaFarbeWert.getText().isEmpty()
+						&& PUNewFilaGewichtWert.getText().isEmpty() && PUNewFilaPreisWert.getText().isEmpty())) {
+					FilamentSpool lNewSpool = new FilamentSpool();
+					lNewSpool.setName(PUNewFilaNameWert.getText());
+					lNewSpool.setColour(PUNewFilaFarbeWert.getText());
+					lNewSpool.setManufacturer(PUNewFilaHerstellerWert.getText());
+					lNewSpool.setIdealNoozleTemp(Integer.parseInt(PUNewFilaNozTempWert.getText()));
+					lNewSpool.setIdealBedTemp(Integer.parseInt(PUNewFilaBedTempWert.getText()));
+					lNewSpool.setWeigth(Integer.parseInt(PUNewFilaGewichtWert.getText()));
+					lNewSpool.setPrice(Double.parseDouble(PUNewFilaPreisWert.getText()));
+					lNewSpool.setDiameter(Double.parseDouble(PUNewFilaDurchmWert.getText()));
+					lNewSpool.setLength(Integer.parseInt(PUNewFilaLaengWert.getText()));
+					lNewSpool.setMaterial((Material) PUNewFilaMaterialWert.getSelectionModel().getSelectedItem());
 //				lNewSpool.set--(Integer.parseInt(PUNewFilaDichteWert.getText()));
-				DatabaseConnection.addFilamentToDatabase(lNewSpool);
-				getStage().close();
+					DatabaseConnection.addFilamentToDatabase(lNewSpool);
+					getStage().close();
+				} else {
+					// popup für Fehlerhinweis
+//					Stage dialog = new Stage();
+//					dialog.initOwner(getStage());
+//					dialog.initModality(Modality.APPLICATION_MODAL);
+//					dialog.showAndWait();
+				}
 			}
 		});
 		mCancelButton.setOnMouseClicked(new EventHandler<Event>() {
